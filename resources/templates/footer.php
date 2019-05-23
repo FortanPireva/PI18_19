9a799fda01ce6c89737938ac4f974277147eb853
FortanPireva <fortanpireva99@gmail.com>
2019-05-23 11:15:49 +0200

    commit

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,12 +1,14 @@
 
 
 <footer>
+    <div id="footer" style="text-align:center;margin-top:15px;">
     <p>
         <?php echo "Aeroporti i Prishtines,Adem Jashari ,&copy".date("Y");?>
         
 
 </p>
 
+    </div>
 </footer>
 </body>
 </html>
