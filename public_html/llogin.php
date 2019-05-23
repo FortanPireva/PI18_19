953e0b4b4e0bb0f971d08640875865780dee41f9
FortanPireva <fortanpireva99@gmail.com>
2019-05-23 11:16:44 +0200

    commit

--- public_html/llogin.php
+++ public_html/llogin.php
@@ -4,6 +4,36 @@ require_once("../resources/config.php");
 
 $css_includes=Array("../css/registration.css");
 echo bootstrap_includes;  
+
+include_once(databaza);
+
+
+$db=new database();
+echo $db->connect();
+
+if($_SERVER['REQUEST_METHOD']=="POST")
+{
+  if(
+     $_POST['email']==""||
+     $_POST['password']=="")
+     {
+        $msgError=htmlspecialchars("Fusha nuk duhet te lihet e zbrazet");
+     }
+  else
+  {
+      $email=$_POST['email'];
+      $password=$_POST['password']; 
+      $query="SELECT emri FROM  user where email='$email' and password='$password';";    
+      $array=array();
+      $array=$db->getData($query);
+  if(count($array)>0)
+      {
+          echo 'Jeni lloguar si: '.$array[0]['emri'];
+      }
+  }
+}
+
+
 include_once(templates_header);
 ?>
 
