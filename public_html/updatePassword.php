3fd55009e0c9b855bae1871bc522697b3fab3d48
FortanPireva <fortanpireva99@gmail.com>
2019-05-28 02:07:29 +0200

    LoginUpdate

--- public_html/updatePassword.php
+++ public_html/updatePassword.php
@@ -1,6 +1,7 @@
 <?php 
-if($_SERVER['REQUEST_METHOD']=="POST"){
   require_once("../resources/config.php");
+if($_SERVER['REQUEST_METHOD']=="POST"){
+
   function generateRandomString($length = 10) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
@@ -14,38 +15,35 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
   $db=new database();
 
 
-  $password=$_POST['pass'];
   $passwordiri=$_POST['conpass'];
   $passwordiriKonfirmo=$_POST['conpass1'];
   $email=$_POST['email'];
 
-  $salt="SELECT salt from user where email='$email'";
+  $query="SELECT salt from user where email='$email'";
+  $salt=$db->getData($query);
   if($passwordiri==$passwordiriKonfirmo)
   {
-      $pass1=sha1($salt.$passwordiri);
+      $pass1=sha1($salt[0]['salt'].$passwordiri);
       $sql="UPDATE user set password='$pass1' where email='$email';";
       $result=$db->executeData($sql);
-      var_dump($result);
+     if($result) 
+      header("Location:llogin.php");
   }
+
 }
+$header_css="../css/style2.css";
+$css_includes="../css/update.css";
+require(templates_header);
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+  <div class="formUpdate">
+    <h3>Ndrysho Passwordin</h3>
   <form  method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
   <input type="email" name="email" placeholder="Sheno email">
-  <input type="password" name="pass" placeholder="Shkruj passing">
   <input type="password" name="conpass" placeholder="Nderro passin">
   <input type="password" name="conpass1" placeholder="Konfirmo passin">
   <input type="submit" value="submit">
   </form>
+  </div>
 </body>
 </html>
