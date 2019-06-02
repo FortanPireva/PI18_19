65238eda98153dd99093225badef1e6315143b13
Gentrit <gentritbiba1@gmail.com>
2019-06-02 08:44:43 +0200

    Perfundimi i email sender, fixes ndesign

--- public_html/index.php
+++ public_html/index.php
@@ -12,25 +12,33 @@ include(templates_header);
 
   
  
-<section class="padded">
-        <h3 class="padded">5 Udhetimet e ardhshme Aeroplan</h3>
-        <form method='Post' action="<?php echo $_SERVER['PHP-SELF']?>">
-            <input type='hidden' id='udhetimiId' name='udhetimiId'>
-            <table class='tabela' cellspacing='0'>
-                <thead>
-                    <th align='left'>Prej</th>
-                    <th align='left'>Deri</th>
-                    <th align='left'>Data</th>
-                    <th style='width: auto;'></th>
-                </thead>
-                <?php 
-                $rez = $db->getData("Select * From flights Where flight_date >= Now() order by flight_date Limit 5");
-                
-                foreach ($rez as $rreshti) {
-                    echo "<tr><td>".$rreshti['origin']."</td><td>".$rreshti['destination']."</td><td>".$rreshti['flight_date']."</td><td style='text-align: center'>"                       
-                    . "<input type='submit' value='Rezervo' class='button button-small id-submit' id='id_".$rreshti['fid']."'></td></tr>";
-                }
-                ?>
-            </table>
-        </form>
+<div class="container">
+    <div class="row">
+    <div class="col-12">
+    <section class="padded">
+            <h3 class="padded">5 Udhetimet e ardhshme Aeroplan</h3>
+            <form method='Post' action="<?php echo $_SERVER['PHP-SELF']?>">
+                <input type='hidden' id='udhetimiId' name='udhetimiId'>
+                <table class='tabela' cellspacing='0'>
+                    <thead>
+                        <th align='left'>Prej</th>
+                        <th align='left'>Deri</th>
+                        <th align='left'>Data</th>
+                        <th style='width: auto;'></th>
+                    </thead>
+                    <?php 
+                    $rez = $db->getData("Select * From flights Where flight_date >= Now() order by flight_date Limit 5");
+                    
+                    foreach ($rez as $rreshti) {
+                        echo "<tr><td>".$rreshti['origin']."</td><td>".$rreshti['destination']."</td><td>".$rreshti['flight_date']."</td><td style='text-align: center'>"                       
+                        . "<input type='submit' class='btn btn-primary btn-rounded mr-2' value='Rezervo' class='button button-small id-submit' id='id_".$rreshti['fid']."'></td></tr>";
+                    }
+                    ?>
+                </table>
+            </form>
+    <section>
+    </div>
+    </div>
+</div>
+        
 <?php include(templates_footer);?>
